07309fdaf4bdbbff25fa97a107315bc695e0f98a
myselfshravan <myselfshravan@gmail.com>
2023-06-29 02:35:24 +0530

    added readme and mode -f

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Streamlit Demo of storing Cookies
+# Streamlit Demo
+
+## How to install
+
+```bash
+pip install streamlit
+streamlit hello
+```
+
+## How to run
+
+```bash
+streamlit run app.py
+```
+
+## How to create requirements.txt
+
+[Reference](https://pip.pypa.io/en/stable/user_guide/#requirements-files)
+
+```bash
+py -m pip install -r requirements.txt
+pip3 freeze >requirements.txt
+```
+
+## [Offical Docs](https://docs.streamlit.io/library/api-reference/write-magic)
+
+
+
--- test.py
+++ test.py
@@ -1,15 +1,13 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.write('''## Welcome to Lost and Found!''')
 
 st.write('''#### Please select an option from the sidebar to get started.''')
 
-st.sidebar.write('''## Options''')
+df = pd.DataFrame(
+    np.random.randn(50, 20),
+    columns=('col %d' % i for i in range(20)))
 
-option = st.sidebar.selectbox(
-    'What would you like to do?',
-    ('Home', 'Lost', 'Found', 'About Us')
-)
-
-if option == 'Home':
-    st.selectbox("what category does your item fall under?", ("Electronics", "Clothing", "Accessories", "Other"))
+st.dataframe(df)
